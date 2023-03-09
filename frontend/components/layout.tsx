c622c7f8e7001178b517c793043c692379eb2c0f
alesun <alesun@exadel.com>
2023-03-09 21:15:26 +0100

    add main pages

---
+++ frontend/components/layout.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Layout(props: { children: any }) {
+    return (
+        <div>
+            <Head>
+                <title>Services Project</title>
+                <link rel="icon" href="#"/>
+                <meta name="keywords" content="services, business"/>
+            </Head>
+            <main>{props.children}</main>
+        </div>
+    );
+}
