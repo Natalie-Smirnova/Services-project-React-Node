512b7a759ccd0d7146aa8dd33215c5b10d71f2b9
aliakseiTraihel <aliaksei.traihel@gmail.com>
2023-04-11 16:28:36 +0200

    refactoring: backend refactoring.

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import connector from "../../../backend/util/mongo-connect";
-import controller from "../../../backend/controller/user-controller";
+import connector from "../../backend/util/mongo-connect";
+import controller from "../../backend/controller/user-controller";
 
 export default function handler(
     req: NextApiRequest,
@@ -9,17 +9,11 @@ export default function handler(
     connector();
     switch (req.method) {
         case 'GET':
-            controller.getUserById(req, res);
+            controller.getUsers(req, res);
             break;
         case 'POST':
             controller.createUser(req, res);
             break;
-        case 'PUT':
-            controller.updateUser(req, res);
-            break;
-        case 'DELETE':
-           controller.deleteUser(req, res);
-           break;
         default:
             return res.status(405).end(`Method ${req.method} Not Allowed`)
     }
